98be57a395870a4a5cd810bc7eb38e66fdcbf8ba
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-21 16:06:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-let fullName = "shivesh richhariya";
-let age = 22;
-age = 45
-age = 56
+// today we are gonna learn and discover console.log() , like how to print
+// in a js file and how to see it in console.log
 
-console.log(fullName, age);
+// today we will also learn about variables , var , let and const and datatypes.
+
+// const fullName = "shivesh richhariya";
+// // fullName = "don";
+// let age = 22;
+// age = 45
+// age = 56
+
+// console.log(fullName, age);
+ 
+// data types 
+
+
+// 1. numbers
+
+// let age = 45;
+// let number = 100;
+
