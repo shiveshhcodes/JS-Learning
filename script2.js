6603463e69daee96e9b988bc13777979c296d129
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-21 18:52:06 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -24,9 +24,9 @@
 // Unary Operators which do not requires two operands like a and b , they can run on single operand as well.
 
 // Increament and Decrement 
-let a = 21;
-let b = 32;
-console.log("a = " , a , "b = " , b)
+// let a = 21;
+// let b = 32;
+// console.log("a = " , a , "b = " , b)
 
 // pre-increment 
 //++a;
@@ -43,5 +43,58 @@ console.log("a = " , a , "b = " , b)
 // post-decrement
 // console.log("a = " , a--);
 // console.log("a = " , a);
+
+
+                            // assignment Operators!!
+
+
+
+// let a = 60;
+// let b = 100;
+
+// a += 50;
+// console.log("a = " , a)
+
+// b -= 50;
+// console.log("b = " , b)
+
+// a *= 50;
+// console.log("a = " , a)
+
+// a **= 5;
+// console.log("a = " , a)
+
+// b /= 50;
+// console.log("b = " , b)
+
+// b %= 50;
+// console.log("b = " , b)
+
+
+                            // Comparision operators
+
+
+let a = 10;
+let b = 21;
+
+// this does not check types!!
+
+console.log("a = " , a , "b = ", b)
+console.log("a == b is" , a==b)
+
+console.log("a = " , a , "b = ", b)
+console.log("a is not equal to b is" , a!=b)
+
+// this does checks the types!!
+
+console.log("a = " , a , "b = ", b)
+console.log("a === b is" , a===b)
+
+console.log("a = " , a , "b = ", b)
+console.log("a type is not equal to b type is - " , a!==b)
+
+
+
+
 
 
