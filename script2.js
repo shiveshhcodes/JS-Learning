548f09451ecebafc095d3d6822345aab6c3ab554
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-21 18:26:36 +0530

    Update script2.js

--- script2.js
+++ script2.js
@@ -2,27 +2,46 @@
 
 // Today we are gonna learns Operators in JavaScript.
 
-// Operators
+                               // Operators
 
-// Arithmetic Operator 
+                           // Arithmetic Operator 
 
-let a = 21;
-let b = 32;
+// let a = 21;
+// let b = 32;
 
-console.log("a = " , a , "b = " , b)
-console.log("a+b = " , a + b);
-console.log("a-b = " , a - b);
-console.log("a*b = " , a * b);
-console.log("a/b = " , a / b);
+// console.log("a = " , a , "b = " , b)
+// console.log("a+b = " , a + b);
+// console.log("a-b = " , a - b);
+// console.log("a*b = " , a * b);
+// console.log("a/b = " , a / b);
 
-// modulus - which gives remainders
-console.log("a%b = " , a % b);
+// // modulus - which gives remainders
+// console.log("a%b = " , a % b);
 
-// Exponentiation - which gives a^b 
-console.log("a**b = " , a**b);
+// // Exponentiation - which gives a^b 
+// console.log("a**b = " , a**b);
 
 // Unary Operators which do not requires two operands like a and b , they can run on single operand as well.
 
+// Increament and Decrement 
+let a = 21;
+let b = 32;
+console.log("a = " , a , "b = " , b)
 
+// pre-increment 
+//++a;
+// console.log("a = " , ++a);
+
+// post-increment
+// console.log("a = " , a++)
+// console.log("a = " , a)
+
+// pre-decrement
+// a--;
+// console.log("a = " , a);
+
+// post-decrement
+// console.log("a = " , a--);
+// console.log("a = " , a);
 
 
