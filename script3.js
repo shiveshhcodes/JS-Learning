a305c5866ac1d3e0ee397ddc8a64b1b560ea93db
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-21 22:01:48 +0530

    Update script3.js

--- script3.js
+++ script3.js
@@ -1,2 +1,16 @@
 // loops!!
 
+// FOR LOOOOOOOP!!!
+
+
+// for (let i=1 ; i<=10 ; i++) {
+//     console.log("Hello India")
+// }
+
+
+// calculate sum from 1 to 5:
+
+let sum = 0;
+for (let i = 1 ; i<=5 ; i+i ) {
+   sum = sum + i;
+}
