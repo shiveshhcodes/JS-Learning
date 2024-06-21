c587381118dc69c9b80e2b236669b7b76ccfc9c5
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-21 17:36:36 +0530

    operators - arithmetic operator

---
+++ script2.js
@@ -0,0 +1,28 @@
+// console.log("helllo JS");
+
+// Today we are gonna learns Operators in JavaScript.
+
+// Operators
+
+// Arithmetic Operator 
+
+let a = 21;
+let b = 32;
+
+console.log("a = " , a , "b = " , b)
+console.log("a+b = " , a + b);
+console.log("a-b = " , a - b);
+console.log("a*b = " , a * b);
+console.log("a/b = " , a / b);
+
+// modulus - which gives remainders
+console.log("a%b = " , a % b);
+
+// Exponentiation - which gives a^b 
+console.log("a**b = " , a**b);
+
+// Unary Operators which do not requires two operands like a and b , they can run on single operand as well.
+
+
+
+
