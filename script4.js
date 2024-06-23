488f0f13104b152af4c8b95db7b45ca97873a7e1
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-24 00:51:02 +0530

    Create script4.js

---
+++ script4.js
@@ -0,0 +1,14 @@
+// console.log(4)
+// for (let i=1 ; i<=34 ; i++){
+//     console.log("value is ",i)
+// }
+
+let sum = 0;
+let n = 12;
+
+for (let c=1;c<=n;c++){
+    sum = sum + c
+}
+console.log(sum)
+
+
