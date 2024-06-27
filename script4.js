8f6990a8cbf8ad94584fba3f25c21a3566506d62
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-27 22:43:30 +0530

    Update script4.js

--- script4.js
+++ script4.js
@@ -1,8 +1,13 @@
 // lets start learning arrays in JS.
 
-// let ar = [ 21 , "shivesh" , "w0w" , 45]
+let ar = [21 , "shivesh" , "w0w" , 45]
 
 // console.log(ar[0]);
 // console.log(ar[1]);
 // console.log(ar[2]);
 
+
+// for loop over. 
+for(let i=0; i<ar.length ; i++){
+    console.log(ar[i])
+}
