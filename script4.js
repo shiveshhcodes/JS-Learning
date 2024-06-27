8d84e27798518ce17431524f70cadba1857360ec
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-06-27 22:46:45 +0530

    Update script4.js

--- script4.js
+++ script4.js
@@ -7,7 +7,12 @@ let ar = [21 , "shivesh" , "w0w" , 45]
 // console.log(ar[2]);
 
 
-// for loop over. 
-for(let i=0; i<ar.length ; i++){
-    console.log(ar[i])
+// for loop. 
+// for(let i=0; i<ar.length ; i++){
+//     console.log(ar[i])
+// }
+
+// for of loop.
+for(let element of ar){
+    console.log(element)
 }
