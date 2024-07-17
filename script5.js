1cd47559641ee7dc94bda0c43995a894529546b5
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-07-17 19:43:01 +0530

    Update script5.js

--- script5.js
+++ script5.js
@@ -1,1 +1,12 @@
-// today we are gonna learn functions and methods in JS
+// today we are gonna learn functions and methods in JS.
+
+function myfunc(){
+    console.log("i am shivesh , i am the best");
+    console.log("i am shivangi , i am the best");
+}
+
+myfunc();
+myfunc();
+myfunc();
+myfunc();
+myfunc();
