e2453412d9d83cc94c151e7577ab4b56afc39a93
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-07-18 19:00:46 +0530

    Update script5.js

--- script5.js
+++ script5.js
@@ -76,8 +76,8 @@ let sq = [1,2,3,4,5,6,7,8,9,10]
 
 // now let discover map method.
 
-let newArr = sq.map((val) => {
-    return val*val;
-});
+// let newArr = sq.map((val) => {
+//     return val*val;
+// });
 
-console.log(newArr)
+// console.log(newArr)
