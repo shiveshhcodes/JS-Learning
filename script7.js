f206a2925952b41a586e6d3c510415a54bb54bb8
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-07-21 01:30:03 +0530

    Update script7.js

--- script7.js
+++ script7.js
@@ -4,3 +4,10 @@
 // id.style.fontSize = "34px";
 // id.innerText = "wow boss";
 // id.style.visibility = "hidden";
+
+let newBtn = document.createElement("button")
+newBtn.innerText = "Click Me";
+console.log(newBtn);
+
+let div = document.querySelector("div");
+div.append(newBtn);
