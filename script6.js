bac087d9c3ca713948c6068d412cedd9c55793ec
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-07-20 14:43:17 +0530

    Update script6.js

--- script6.js
+++ script6.js
@@ -13,8 +13,18 @@
 // let div = document.querySelector("div");
 // console.dir(div)
 
-let h2 = document.querySelector("h2")
-console.dir(h2.innerText);
+// let h2 = document.querySelector("h2")
+// console.dir(h2.innerText);
 
-h2.innerText = h2.innerText + " from shivesh Richhariya";
+// h2.innerText = h2.innerText + " from shivesh Richhariya";
 
+let divs = document.querySelectorAll(".box");
+// divs[0].innerText = divs[0].innerText + " new unique value of 1 "
+// divs[1].innerText = divs[1].innerText + " new unique value of 2 "
+// divs[2].innerText = divs[2].innerText + " new unique value of 3 "
+
+let idx = 1;
+for( div of divs) {
+    div.innerText = `new unique value is ${idx}`
+    idx++;
+}
