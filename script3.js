52886bee1c554029200ec038cf1ebd64ca9c7dff
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-07-22 23:57:34 +0530

    Update script3.js

--- script3.js
+++ script3.js
@@ -111,7 +111,7 @@
 
 // console.log(newstr)
 
-let fullName = prompt("enter your Full name");
+let fullName = prompt("enter your Full Name");
 // username = "@" + fullName + fullName.length
 // console.log(username)
 
