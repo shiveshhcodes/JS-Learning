16fe4bc29fc74338a52e9a034457d1c5bb05f957
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-07-25 02:24:19 +0530

    Update script8.js

--- script8.js
+++ script8.js
@@ -1,12 +1,47 @@
-let btn1 = document.querySelector('#btn1');
+// let btn1 = document.querySelector('#btn1');
 
-btn1.onclick = (e) => {
-    console.log(e)
-}
-let a = 0
-let btn2 = document.querySelector('#btn2')
-btn2.onmouseover = () => {
-    a++;
-    console.log(`btn was clicked twice and total of ${a} times.`)
+// btn1.onclick = (e) => {
+//     console.log(e)
+// }
+// let a = 0
+// let btn2 = document.querySelector('#btn2')
+// btn2.onmouseover = () => {
+//     a++;
+//     console.log(`btn was clicked twice and total of ${a} times.`)
 
-}
+// }
+
+// btn1.addEventListener("click" , () =>{
+//     console.log("button was printed")
+// })
+
+// qs
+
+// let modeBtn = document.querySelector("#mode")
+// let currMode = "light";
+// modeBtn.addEventListener("click" , () =>{
+//      if (currMode ==="light") {
+//         currMode = "dark"
+//         document.querySelector("body").style.backgroundColor = "black";
+//      }
+//      else {
+//         currMode = "light"
+//         document.querySelector("body").style.backgroundColor = "white";
+//      }
+//      console.log(currMode);
+// })
+let modeBtn = document.querySelector("#mode");
+let body = document.querySelector("body");
+let currMode = "light";
+modeBtn.addEventListener("click" , () =>{
+     if (currMode ==="light") {
+        currMode = "dark"
+        
+     }
+     else {
+        currMode = "light"
+    
+     }
+     console.log(currMode);
+})
+
