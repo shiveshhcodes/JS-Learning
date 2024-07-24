f3052265913c5a68607737ec44ed98c5e9b33cd3
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-07-24 18:53:21 +0530

    Update script7.js

--- script7.js
+++ script7.js
@@ -5,21 +5,24 @@
 // id.innerText = "wow boss";
 // id.style.visibility = "hidden";
 
-// let newBtn = document.createElement("button")
-// newBtn.innerText = "Click MeE";
-// console.log(newBtn);
+let newBtn = document.createElement("button")
+newBtn.innerText = "Click MeE";
+console.log(newBtn);
 
-// let div = document.querySelector("div");
-// // div.append(newBtn);
-// div.before(newBtn);
+let div = document.querySelector("div");
+// div.append(newBtn);
+div.before(newBtn);
+newBtn.remove();
 
 
-let newHeading = document.createElement("h2");
-newHeading.innerHTML = "<i> Hello Shivesh </i>";
-console.log(newHeading);
 
-let p = document.querySelector("body").prepend(newHeading)
+// let newHeading = document.createElement("h2");
+// newHeading.innerHTML = "<i> Hello Shivesh </i>";
+// console.log(newHeading);
+
+// let p = document.querySelector("body").prepend(newHeading)
 // p.prepend(newHeading);
 // p.append(newHeading);
 // p.before(newHeading)
 // p.after(newHeading)
+
