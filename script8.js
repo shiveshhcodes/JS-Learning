0a0ae3671fe5ea1b6321fa49c971b7ac56e561f4
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-07-25 00:44:33 +0530

    Update script8.js

--- script8.js
+++ script8.js
@@ -3,8 +3,10 @@ let btn1 = document.querySelector('#btn1');
 btn1.onclick = () => {
     console.log("btn1 was click")
 }
-
+let a = 0
 let btn2 = document.querySelector('#btn2')
 btn2.onclick = () => {
-    console.log("btn was clicked twice")
+    a++;
+    console.log(`btn was clicked twice and total of ${a} times.`)
+
 }
