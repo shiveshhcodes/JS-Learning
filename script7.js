d83ce4b010e627dab44dcbb784ef8d6cc9ee5eff
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-07-24 19:12:46 +0530

    Update script7.js

--- script7.js
+++ script7.js
@@ -33,4 +33,9 @@ let newBtn = document.createElement("button");
 newBtn.innerText = "click me";
 console.log(newBtn);
 
-let body = document.querySelector("body").prepend(newBtn)
+let body = document.querySelector("body");
+body.prepend(newBtn)
+
+newBtn.style.backgroundColor = "red";
+newBtn.style.color = "white";
+
