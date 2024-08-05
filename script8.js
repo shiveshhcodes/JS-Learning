06a6b00a654316987664e394acde791ed5758617
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-08-05 23:59:00 +0530

    Update script8.js

--- script8.js
+++ script8.js
@@ -34,14 +34,13 @@ let modeBtn = document.querySelector("#mode");
 let body = document.querySelector("body");
 let currMode = "light";
 modeBtn.addEventListener("click" , () =>{
-     if (currMode ==="light") {
+     if (currMode === "light") {
         currMode = "dark"
         body.classList.add("dark")
      }
      else {
         currMode = "light"
         body.classList.add("light")
-    
      }
      console.log(currMode);
 })
