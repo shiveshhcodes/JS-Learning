2665b91789a4c32150e3910f0f05d49c5bef2a64
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-08-06 20:59:18 +0530

    Create script9.js

---
+++ script9.js
@@ -0,0 +1,8 @@
+// today we are gonna learn classes and objects
+const student = {
+    fullName : "shivesh Richhariya" ,
+    Marks : "95.3" ,
+    printMarks : function(){
+        console.log("marks = " , this.Marks);
+    }
+}
