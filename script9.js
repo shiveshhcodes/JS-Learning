57613aff37f50fc0c1b61f5253fd308a87fa23f3
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-08-07 02:39:35 +0530

    Update script9.js

--- script9.js
+++ script9.js
@@ -1,14 +1,16 @@
 // today we are gonna learn classes and objects
-const student = {
-    fullName : "shivesh Richhariya" ,
-    Marks : "95.3" ,
-    printMarks : function(){
-        console.log("marks is = " , this.Marks);
-    }
-}
 
-const shivesh = {
-    Marks: 21,
-};
+// this is what and where prototypes are used!!
+// const student = {
+//     fullName : "shivesh Richhariya" ,
+//     Marks : "95.3" ,
+//     printMarks : function(){
+//         console.log("marks is = " , this.Marks);
+//     }
+// }
 
-shivesh.__proto__ = student;
+// const shivesh = {
+//     Marks: 21,
+// };
+
+// shivesh.__proto__ = student;
