83c3f991e0535830d54090fe43fb3628a3ad088a
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-08-11 22:42:15 +0530

    Update script10.js

--- script10.js
+++ script10.js
@@ -28,5 +28,12 @@ function getData(dataID , getNextData) {
         }
     } , 1000);
 }
-
-getData(4)
+getData(1,() =>{
+ getData(2,()=>{
+  getData(3,()=>{
+    getData(4,()=>{
+        getData(5)
+    })
+  })
+ })
+})
