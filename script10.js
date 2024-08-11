d13454b0d2add876eea06c1d6a686a6c6622e85b
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-08-11 22:39:21 +0530

    Update script10.js

--- script10.js
+++ script10.js
@@ -8,6 +8,17 @@
 
 // setTimeout(sum(24,23) , 3000)
 
+// function getData(dataID , getNextData) {
+//     // 2s
+//     setTimeout(() => {
+//         console.log("data is" , dataID)
+//         if (dataID < 689) {
+//             getData(dataID+4 , getNextData);
+//         }
+//     } , 1);
+// }
+
+// getData(4)
 function getData(dataID , getNextData) {
     // 2s
     setTimeout(() => {
