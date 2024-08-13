eba1b0c3a0da2c695986b00ebfe05d615d96d503
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-08-13 21:59:04 +0530

    Update script10.js

--- script10.js
+++ script10.js
@@ -19,21 +19,26 @@
 // }
 
 // getData(4)
-function getData(dataID , getNextData) {
-    // 2s
-    setTimeout(() => {
-        console.log("data is" , dataID)
-        if (getNextData) {
-            getNextData();
-        }
-    } , 1000);
-}
-getData(1,() =>{
- getData(2,()=>{
-  getData(3,()=>{
-    getData(4,()=>{
-        getData(5)
-    })
-  })
- })
+// function getData(dataID , getNextData) {
+//     // 2s
+//     setTimeout(() => {
+//         console.log("data is" , dataID)
+//         if (getNextData) {
+//             getNextData();
+//         }
+//     } , 200);
+// }
+// getData(1,() =>{
+//  getData(2,()=>{
+//   getData(3,()=>{
+//     getData(4,()=>{
+//         getData(5)
+//     })
+//   })
+//  })
+// })
+
+let promise = new Promise((resolve , rejected) =>{
+    console.log("it is a success")
+    resolve("it's an hit")
 })
