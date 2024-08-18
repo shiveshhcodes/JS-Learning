7bf96b53e505fcbac9027453cac5320e4ac58125
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-08-19 01:55:46 +0530

    Update script11.js

--- script11.js
+++ script11.js
@@ -2,23 +2,23 @@ const URL = "https://cat-fact.herokuapp.com/facts"
 const factPara = document.querySelector("#fact")
 const button = document.querySelector("#btn")
 
-// const getFacts = async () => {
-//     console.log("getting data....")
-//     let response = await fetch(URL)
-//     console.log(response.status)
-//     let data = await response.json();
-//     factPara.innerText = data[3].text;
-// }
-
-function getFacts() {
-    console.log("fetching data....");
-    fetch(URL)
-    .then((response) => {
-      return response.json();
-    }).then((data) =>{
-     console.log(data);
-     factPara.innerText = data[2].text;    
-    })
+const getFacts = async () => {
+    console.log("getting data....")
+    let response = await fetch(URL)
+    console.log(response.status)
+    let data = await response.json();
+    factPara.innerText = data[3].text;
 }
+
+// function getFacts() {
+//     console.log("fetching data....");
+//     fetch(URL)
+//     .then((response) => {
+//       return response.json();
+//     }).then((data) =>{
+//      console.log(data);
+//      factPara.innerText = data[2].text;    
+//     })
+// }
 
 button.addEventListener("click" , getFacts)
