4cb879820cec4f8d3572eb4413070e99024f19d7
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-08-19 01:25:36 +0530

    Update script11.js

--- script11.js
+++ script11.js
@@ -1,6 +1,7 @@
 const URL = "https://cat-fact.herokuapp.com/facts"
 const factPara = document.querySelector("#fact")
 const button = document.querySelector("#btn")
+
 // const getFacts = async () => {
 //     console.log("getting data....")
 //     let response = await fetch(URL)
