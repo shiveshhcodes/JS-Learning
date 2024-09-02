1b07e87fd90452c4663c4468a0e6b9be0a851f51
Shivesh Richhariya <shiveshdeveloper@gmail.com>
2024-09-02 23:58:26 +0530

    Update script10.js

--- script10.js
+++ script10.js
@@ -153,7 +153,7 @@ function getData(dataID){
 (async function() {
     console.log("getting data 1....")
     await getData(1)
-    console.log("getting data 2...,.")
+    console.log("getting data 2....")
     await getData(2)
     console.log("getting data 3.....")
     await getData(3)
